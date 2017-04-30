3549f271b45ce90b2c5de96bf59b2e9aef15191f
Madalina Ciortan <ciortanmadalina@gmail.com>
2017-04-30 18:55:14 +0200

    first draft

--- algo.py
+++ algo.py
@@ -1,46 +1,68 @@
-class Node:
-    state=0;
-    nextStatesByWeight= {};
 
-    def __init__(self, state):
-        self.state = state
+#Global variables
+g = {} #this dictionary represents go to function and holds as keys tuples (state, weight) and the value is the next state
+output = {} #this dictionary maps states to a list of unique keywords found
+failure = {} # this dictionary maps each state with the next state
+caseSensitive = False # when this parameter is set to false, we match strings with letter case difference
 
 
-    def __repr__(self):
-        '''
-        Override to string method to get some meaningful debug messages
-        :return:
-        '''
+fail = None # global constant with a meaningful name which is the output of the graph f when the weight(letter) is not part of the graph
+newstate = 0 #global variable used to propagate the state index cross kewords iterations
 
-        return 'State : %s, weights : %s \n' % (self.state, self.nextStatesByWeight)
+def initializeGlobalParameters():
+    ''''
+    This method makes sure that all global parameters are properly reset
+    '''
+    global g
+    global output
+    global failure
+    global newstate
+    g = {}
+    output = {}
+    failure = {}
+    newstate = 0
 
-    def addStateWithWeight(self, nextState, nextStateWeight):
-        self.nextStatesByWeight[nextState] = nextStateWeight
+def createStateMachine(keywords):
+    ''''
+    This method initializes global parameters, creates the go to, failure and output functions
+    '''
+    initializeGlobalParameters()
+    createGoToFunction(keywords)
+    createFailueFunction()
 
 
 def createGoToFunction(keywords):
-    newstate = 0
+    '''
+
+    :param keywords: list of strings based on which we create the automata
+    :return: go to function
+    '''
+
+    newstate = 0 # start with state 0
     for keyword in keywords:
         processKeyword(keyword)
 
+    # For all letters which are not mapped to the 0 root state, create references to state 0
+    # For efficiency concatenate all keywords and remove duplicated letters
     allkeys = ''.join(set(''.join(keywords)))
     for i in range(len(allkeys)):
         if g.get((0, allkeys[i]), fail) == fail:
             g[(0, allkeys[i])] = 0
-
-
-g = {}
-output = {}
-failure = {}
-newstate = 0
-fail = None
+    return g
 
 
 def processKeyword(keyword):
+    '''
+    For each letter in keyword, if it is already present in our graph g at current state,
+    update current state to the value of the next state
+    otherwise add a new node to the graph mapping the current state + 1 with the current letter
+    :param keyword:
+    :return:
+    '''
     global newstate
     state = 0
     j = 0
-    while g.get((state, keyword[j]), fail) != fail:
+    while g.get((state, keyword[j]), fail) != fail: #state already defined
         state = g[(state, keyword[j])] #we are sure the key exists
         j = j + 1
 
@@ -51,9 +73,18 @@ def processKeyword(keyword):
 
     updateOutput(state, keyword)
 
-    return keyword
 
 def updateOutput(state, keyword) :
+    '''
+    This method updates output dictionary with value keyword at key state.
+    Because we can find multiple keywords at the same index, output maintains a list of
+    keywords.
+    For this reason, if input keyword is a string and the key doest't exist, I create an array
+    with given value, otherwise I add keyword to existing array.
+    This method is called by processKeyword and createFailure function and in the latter case
+    it may pass a list of values (the payload of another output node, for this reason
+    I check the instance type and do an array extends.
+    '''
     if state in output.keys():
         #this scenario will be triggered by updates from create failure
         if isinstance(keyword, list):
@@ -65,6 +96,10 @@ def updateOutput(state, keyword) :
 
 
 def createFailueFunction():
+    '''
+    This method iterates through all letters of keywords payload (set method guarantees
+    distinct output) and uses a queue to manange states.
+    '''
     queue = []
     allkeys = ''.join(set(''.join(keywords)))
     for i in range(len(allkeys)):
@@ -91,25 +126,48 @@ def createFailueFunction():
 
 
 def findInString(inputString):
+    '''
+    Find all occurences of keywords in given inputString
+    This method has to be called after createStateMachine
+    '''
     state = 0
     for i in range(len(inputString)) :
-
+        #failure function doesn't have a value for state 0 therefor make sure the
+        #this loop doesn't execute the very first time
         while g.get((state, inputString[i]), fail) == fail and state != 0:
-            #print ('STATE = ', state)
             state = failure[state]
+        #update state only when the tuple state, inputString[i] exists in go to graph, otherwise keep existing state
         state = g.get((state, inputString[i]), state)
         if output.get(state, None ) != None :
-            print('i = %s , output = %s , actual : %s' % (i, output[state], inputString[:i + 1]))
+            print('\nIndex : %s , Output keywords %s \nActual text : %s' % (i, output[state], inputString[:i + 1]))
+
+
+def readTextFromFile(filename):
+    '''
+    Return string payload of input filename.
+
+    There might be differences in the output index results due to extra characters passed
+    along during the copy paste to the input file.
+    '''
+    with open(filename, 'r') as myfile:
+        text = myfile.read()
+        if not caseSensitive :
+            return text.lower()
+        return text
+
+
 
 keywords = ['he', 'she', 'his', 'hers']
 text = 'ushers'
 
 keywords = ['pattern',	'tree',	'state','prove','the','it']
-with open('input', 'r') as myfile:
-    text=myfile.read().replace('\n', '')
+if caseSensitive :
+    keywords = [x.lower() for x in keywords]
 
-createGoToFunction(keywords)
-createFailueFunction()
-print( 'G = %s \n Ouput %s \nFailure %s' % (g, output, failure))
+text = readTextFromFile('input')
+
+createStateMachine(keywords)
+print( '>Go To Graph : %s \n>Ouput function : %s \n>Failure function : %s' % (g, output, failure))
+
 
 findInString(text)
